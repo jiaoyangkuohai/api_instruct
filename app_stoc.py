87c8ae4d07e880f02026f9447df6055fd151470d
shihongliang <shihongliangguo@163.com>
2024-10-05 23:10:23 +0800

    添加version和下载功能

--- app_stoc.py
+++ app_stoc.py
@@ -4,32 +4,20 @@ import streamlit as st
 import re
 
 def main():
+    with open("version.txt", "r", encoding="utf-8") as file:
+        version = file.read()
     # 读取 Markdown 文件
     with open("openai_api_instruct.md", "r", encoding="utf-8") as file:
         markdown_text = file.read()
     stoc.from_markdown(markdown_text)
 
+    with open("openai_api_instruct.pdf", "rb") as pdf_file:
+        PDFbyte = pdf_file.read()
+
+    st.sidebar.download_button(label="下载离线PDF文档",
+                        data=PDFbyte,
+                        file_name=f"算法接口说明文档_{str(version)}.pdf",
+                        mime='application/octet-stream')
+
 if __name__ == "__main__": 
-    # md = """
-    # # Demo
-    # sdfed
-
-    # ## I want to talk about this
-    # sdef
-
-    # ### Smaller again
-    # ijfds
-
-    # ## Another subtitle
-    # 水电费
-
-    # ### I also should address that
-    # 暖气费
-
-    # ## Conclusion
-    # 总计
-    # """
-
-    # # stoc.from_markdown(md)
-    # stoc.from_markdown(md)
     main()
